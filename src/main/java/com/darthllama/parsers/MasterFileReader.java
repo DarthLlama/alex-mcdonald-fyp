fa2932677a42b7d4ce97bd6e4c1e78bf75210d9f
Alex McDonald <macdiiv@gmail.com>
2019-01-30 17:10:56 +0000

    Who knew having the file extensions was important? JSON files are now .json files, and the program can now move the files from the internal file structure to the users directory on both installation and when directed to.

--- src/main/java/com/darthllama/parsers/MasterFileReader.java
+++ src/main/java/com/darthllama/parsers/MasterFileReader.java
@@ -16,11 +16,12 @@ public class MasterFileReader {
 
     final static Logger logger = Logger.getLogger(MasterFileReader.class);
 
+    static List<String> backgroundList = new ArrayList<>();
     static List<String> classList = new ArrayList<>();
+    static List<String> equipmentList = new ArrayList<>();
     static List<String> featureList = new ArrayList<>();
     static List<String> raceList = new ArrayList<>();
     static List<String> spellsList = new ArrayList<>();
-    //TODO Equipment
 
     public static void ReadFiles(){
         //File path
@@ -31,15 +32,47 @@ public class MasterFileReader {
                 Application.props.getProperty("resourceFolderPath");
 
         //each of these files contains a list of files that have a corresponding JSON file
-        String class_list_file = "\\resources\\classes\\00class_list.txt";
-        String feature_list_file = "\\resources\\features\\00features_list.txt";
-        String race_list_file = "\\resources\\races\\00race_list.txt";
-        String spells_list_file = "\\resources\\spells\\00spells_list.txt";
+        String background_list_file = "\\resources\\json\\backgrounds\\00background_list.txt";
+        String class_list_file = "\\resources\\json\\classes\\00class_list.txt";
+        String equipment_list_file = "\\resources\\json\\equipment\\00equipment_list.txt";
+        String feature_list_file = "\\resources\\json\\features\\00features_list.txt";
+        String race_list_file = "\\resources\\json\\races\\00race_list.txt";
+        String spells_list_file = "\\resources\\json\\spells\\00spells_list.txt";
+
+        //read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(filePath + background_list_file))) {
+            //stream.forEach(System.out::println);
+            backgroundList = stream
+                    .filter(line -> !line.startsWith("//")) //allows comments
+                    .filter(line -> !line.isEmpty())   //Removes white space lines
+                    .map(String::toLowerCase)       //removes the problem of cases
+                    .distinct()                     //removes duplicates
+                    .collect(Collectors.toList());  //Adds it to the list
+
+        } catch (IOException e) {
+            logger.error("IOException:" + e);
+            e.printStackTrace();
+        }
 
         //read file into stream, try-with-resources
         try (Stream<String> stream = Files.lines(Paths.get(filePath + class_list_file))) {
             //stream.forEach(System.out::println);
             classList = stream
+                    .filter(line -> !line.startsWith("//")) //allows comments
+                    .filter(line -> !line.isEmpty())   //Removes white space lines
+                    .map(String::toLowerCase)       //removes the problem of cases
+                    .distinct()                     //removes duplicates
+                    .collect(Collectors.toList());  //Adds it to the list
+
+        } catch (IOException e) {
+            logger.error("IOException:" + e);
+            e.printStackTrace();
+        }
+
+        //read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(filePath + equipment_list_file))) {
+            //stream.forEach(System.out::println);
+            equipmentList = stream
                     .filter(line -> !line.startsWith("//")) //allows comments
                     .filter(line -> !line.isEmpty())   //Removes white space lines
                     .map(String::toLowerCase)       //removes the problem of cases
@@ -97,44 +130,64 @@ public class MasterFileReader {
         }
 
         //TODO: Parse each and everyone of these
-        logger.info("Class lists: " + classList);
-        logger.info("Feature lists: " + featureList);
-        logger.info("Race lists: " + raceList);
-        logger.info("Spell lists: " + spellsList);
+        logger.info("Background list: " + backgroundList);
+        logger.info("Class list: " + classList);
+        logger.info("Equipment list: " + equipmentList);
+        logger.info("Feature list: " + featureList);
+        logger.info("Race list: " + raceList);
+        logger.info("Spell list: " + spellsList);
 
         //Check that all the names listed have related files
         int tempCount =0;
         while(tempCount < classList.size()){
-            File file = new File(filePath + "\\resources\\classes\\" + classList.get(tempCount) + ".txt");
+            File file = new File(filePath + "\\resources\\json\\backgrounds\\" + backgroundList.get(tempCount) + ".json");
             if(!file.exists()){
-                logger.error(classList.get(tempCount) + ".txt does not exist");
+                logger.error(backgroundList.get(tempCount) + ".json does not exist");
+            }
+            tempCount++;
+        }
+
+        tempCount =0;
+        while(tempCount < classList.size()){
+            File file = new File(filePath + "\\resources\\json\\classes\\" + classList.get(tempCount) + ".json");
+            if(!file.exists()){
+                logger.error(classList.get(tempCount) + ".json does not exist");
+            }
+            tempCount++;
+        }
+
+        tempCount =0;
+        while(tempCount < equipmentList.size()){
+            File file = new File(filePath + "\\resources\\json\\equipment\\" + equipmentList.get(tempCount) + ".json");
+            if(!file.exists()){
+                logger.error(equipmentList.get(tempCount) + ".json does not exist");
             }
             tempCount++;
         }
 
         tempCount = 0;
         while(tempCount < featureList.size()){
-            File file = new File(filePath + "\\resources\\features\\" + featureList.get(tempCount) + ".txt");
+            File file = new File(filePath + "\\resources\\json\\features\\" + featureList.get(tempCount) + ".json");
             if(!file.exists()){
-                logger.error(featureList.get(tempCount) + ".txt does not exist");
+                logger.error(featureList.get(tempCount) + ".json does not exist");
             }
             tempCount++;
         }
 
         tempCount = 0;
         while(tempCount < raceList.size()){
-            File file = new File(filePath + "\\resources\\features\\" + raceList.get(tempCount) + ".txt");
+            File file = new File(filePath + "\\resources\\json\\races\\" + raceList.get(tempCount) + ".json");
             if(!file.exists()){
-                logger.error(raceList.get(tempCount) + ".txt does not exist");
+                logger.error(raceList.get(tempCount) + ".json does not exist");
             }
             tempCount++;
         }
 
         tempCount = 0;
         while(tempCount < spellsList.size()){
-            File file = new File(filePath + "\\resources\\features\\" + spellsList.get(tempCount) + ".txt");
+            File file = new File(filePath + "\\resources\\json\\spells\\" + spellsList.get(tempCount) + ".json");
             if(!file.exists()){
-                logger.error(spellsList.get(tempCount) + ".txt does not exist");
+                logger.error(spellsList.get(tempCount) + ".json does not exist");
             }
             tempCount++;
         }
--- src/main/java/com/darthllama/utilities/WriteJSONFiles.java
+++ src/main/java/com/darthllama/utilities/WriteJSONFiles.java
@@ -20,13 +20,16 @@ public class WriteJSONFiles {
     private final static Logger logger = Logger.getLogger(WriteJSONFiles.class);
 
     //Collections
+    private static List<String> backgroundList = new ArrayList<>();
     private static List<String> classList = new ArrayList<>();
+    private static List<String> equipmentList = new ArrayList<>();
     private static List<String> featureList = new ArrayList<>();
     private static List<String> raceList = new ArrayList<>();
     private static List<String> spellsList = new ArrayList<>();
 
     public void WriteDefaultJSONFiles(String filePath) {
         //logger.info(getFile("json/classes/00class_list.txt"));
+        //logger.info(getFile("json/classes/cleric.json"));
 
         //Its all well and good having these resource files in the resources folder, but we need hard copies
         //on the users machine. This is how.
@@ -34,29 +37,62 @@ public class WriteJSONFiles {
         logger.info("Resources not found. Creating...");
 
         //Makes the folders
-        File dir = new File(filePath + "\\alex-mcdonald-fyp\\resources\\classes");
+        File dir = new File(filePath + "\\alex-mcdonald-fyp\\resources\\json\\backgrounds");
         dir.mkdirs();
-            dir = new File(filePath + "\\alex-mcdonald-fyp\\resources\\features");
+            dir = new File(filePath + "\\alex-mcdonald-fyp\\resources\\json\\classes");
         dir.mkdirs();
-            dir = new File(filePath + "\\alex-mcdonald-fyp\\resources\\races");
+            dir = new File(filePath + "\\alex-mcdonald-fyp\\resources\\json\\equipment");
         dir.mkdirs();
-            dir = new File(filePath + "\\alex-mcdonald-fyp\\resources\\spells");
+            dir = new File(filePath + "\\alex-mcdonald-fyp\\resources\\json\\features");
+        dir.mkdirs();
+            dir = new File(filePath + "\\alex-mcdonald-fyp\\resources\\json\\races");
+        dir.mkdirs();
+            dir = new File(filePath + "\\alex-mcdonald-fyp\\resources\\json\\spells");
         dir.mkdirs();
         //config.application
             dir = new File(filePath + "\\alex-mcdonald-fyp");
         dir.mkdirs();
 
         //TODO: I really have to finish creating the JSON files to be able to finish testing this
-        writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\classes\\00class_list.txt", getFile("json/classes/00class_list.txt"));
-        //writeFile(getFile(filePath + "\\alex-mcdonald-fyp\\resources\features\\00features_list.txt"),"json/features/00features_list.txt"));
-        //writeFile(getFile(filePath + "\\alex-mcdonald-fyp\\resources\races\\00race_list.txt"),"json/races/00race_list.txt"));
-        //writeFile(getFile(filePath + "\\alex-mcdonald-fyp\\resources\spells\\00spells_list.txt"),"json/spells/00spells_list.txt"));
+        logger.info("Copying the Class files from the resources into the user directory; starting with 00class_list");
+        writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\backgrounds\\00background_list.txt", getFile("json/backgrounds/00background_list.txt"));
+        writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\classes\\00class_list.txt", getFile("json/classes/00class_list.txt"));
+        writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\equipment\\00equipment_list.txt", getFile("json/equipment/00equipment_list.txt"));
+        writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\features\\00features_list.txt",getFile("json/features/00features_list.txt"));
+        writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\races\\00race_list.txt",getFile("json/races/00race_list.txt"));
+        writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\spells\\00spells_list.txt",getFile("json/spells/00spells_list.txt"));
         //writeFile(filePath + "\\alex-mcdonald-fyp\\config.properties",getFile("config.properties"));
 
 
-        //TODO: Do this for Races, Features & Spells
+        //TODO: Just Features and spells left boi
+        //TODO: Is there a way to optimise this?
+        //Backgrounds
         //read file into stream, try-with-resources
-        try (Stream<String> stream = Files.lines(Paths.get(filePath + "\\alex-mcdonald-fyp\\resources\\classes\\00class_list.txt"))) {
+        try (Stream<String> stream = Files.lines(Paths.get(filePath + "\\alex-mcdonald-fyp\\resources\\json\\backgrounds\\00background_list.txt"))) {
+            //stream.forEach(System.out::println);
+            backgroundList = stream
+                    .filter(line -> !line.startsWith("//")) //allows comments
+                    .filter(line -> !line.isEmpty())   //Removes white space lines
+                    .map(String::toLowerCase)       //removes the problem of cases
+                    .distinct()                     //removes duplicates
+                    .collect(Collectors.toList());  //Adds it to the list
+
+        } catch (IOException e) {
+            logger.error("IOException:" + e);
+            e.printStackTrace();
+        }
+
+        //Check that all the names listed have related files
+        int tempCount = 0;
+        while (tempCount < backgroundList.size()) {
+            logger.info("Going through the background list: " + backgroundList.get(tempCount) + ".json");
+            writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\backgrounds\\" + backgroundList.get(tempCount) + ".json", getFile("json/backgrounds/" + backgroundList.get(tempCount) + ".json"));
+            tempCount++;
+        }
+
+        //Classes
+        //read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(filePath + "\\alex-mcdonald-fyp\\resources\\json\\classes\\00class_list.txt"))) {
             //stream.forEach(System.out::println);
             classList = stream
                     .filter(line -> !line.startsWith("//")) //allows comments
@@ -71,21 +107,123 @@ public class WriteJSONFiles {
         }
 
         //Check that all the names listed have related files
-        int tempCount = 0;
+        tempCount = 0;
         while (tempCount < classList.size()) {
-            getFile("json/classes/" + classList.get(tempCount) + ".txt");
-            writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\classes\\" + classList.get(tempCount) + ".txt", getFile("json/classes/" + classList.get(tempCount) + ".txt"));
+            logger.info("Going through the class list: " + classList.get(tempCount) + ".json");
+            //getFile("C:\\Users\\Alex\\Documents\\Programming\\Projects\\alex-mcdonald-fyp\\src\\main\\resources\\json\\classes\\" + classList.get(tempCount) + ".txt");
+            writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\classes\\" + classList.get(tempCount) + ".json", getFile("json/classes/" + classList.get(tempCount) + ".json"));
+            //writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\classes\\00class_list.txt", getFile("json/classes/00class_list.txt"));
             tempCount++;
         }
 
+        //Equipment
+        //read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(filePath + "\\alex-mcdonald-fyp\\resources\\json\\equipment\\00equipment_list.txt"))) {
+            //stream.forEach(System.out::println);
+            equipmentList = stream
+                    .filter(line -> !line.startsWith("//")) //allows comments
+                    .filter(line -> !line.isEmpty())   //Removes white space lines
+                    .map(String::toLowerCase)       //removes the problem of cases
+                    .distinct()                     //removes duplicates
+                    .collect(Collectors.toList());  //Adds it to the list
+
+        } catch (IOException e) {
+            logger.error("IOException:" + e);
+            e.printStackTrace();
+        }
+
+        //Check that all the names listed have related files
+        tempCount = 0;
+        while (tempCount < equipmentList.size()) {
+            logger.info("Going through the equipment list: " + equipmentList.get(tempCount) + ".json");
+            writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\equipment\\" + equipmentList.get(tempCount) + ".json", getFile("json/equipment/" + equipmentList.get(tempCount) + ".json"));
+            tempCount++;
+        }
+        /*TODO: Writing to the drive Features JSON
+        //Features
+        //read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(filePath + "\\alex-mcdonald-fyp\\resources\\json\\features\\00features_list.txt"))) {
+            //stream.forEach(System.out::println);
+            featureList = stream
+                    .filter(line -> !line.startsWith("//")) //allows comments
+                    .filter(line -> !line.isEmpty())   //Removes white space lines
+                    .map(String::toLowerCase)       //removes the problem of cases
+                    .distinct()                     //removes duplicates
+                    .collect(Collectors.toList());  //Adds it to the list
+
+        } catch (IOException e) {
+            logger.error("IOException:" + e);
+            e.printStackTrace();
+        }
+
+        //Check that all the names listed have related files
+        tempCount = 0;
+        while (tempCount < featureList.size()) {
+            logger.info("Going through the features list: " + featureList.get(tempCount) + ".json");
+            writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\features\\" + featureList.get(tempCount) + ".json", getFile("json/features/" + featureList.get(tempCount) + ".json"));
+            tempCount++;
+        }
+        */
+
+        //Races
+        //read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(filePath + "\\alex-mcdonald-fyp\\resources\\json\\races\\00race_list.txt"))) {
+            //stream.forEach(System.out::println);
+            raceList = stream
+                    .filter(line -> !line.startsWith("//")) //allows comments
+                    .filter(line -> !line.isEmpty())   //Removes white space lines
+                    .map(String::toLowerCase)       //removes the problem of cases
+                    .distinct()                     //removes duplicates
+                    .collect(Collectors.toList());  //Adds it to the list
+
+        } catch (IOException e) {
+            logger.error("IOException:" + e);
+            e.printStackTrace();
+        }
+
+        //Check that all the names listed have related files
+        tempCount = 0;
+        while (tempCount < raceList.size()) {
+            logger.info("Going through the race list: " + raceList.get(tempCount) + ".json");
+            writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\races\\" + raceList.get(tempCount) + ".json", getFile("json/races/" + raceList.get(tempCount) + ".json"));
+            tempCount++;
+        }
+
+        //TODO: Write Spells JSON to the users drive
+        /*
+        //Equipment
+        //read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(filePath + "\\alex-mcdonald-fyp\\resources\\json\\spells\\00spell_list.txt"))) {
+            //stream.forEach(System.out::println);
+            spellsList = stream
+                    .filter(line -> !line.startsWith("//")) //allows comments
+                    .filter(line -> !line.isEmpty())   //Removes white space lines
+                    .map(String::toLowerCase)       //removes the problem of cases
+                    .distinct()                     //removes duplicates
+                    .collect(Collectors.toList());  //Adds it to the list
+
+        } catch (IOException e) {
+            logger.error("IOException:" + e);
+            e.printStackTrace();
+        }
+
+        //Check that all the names listed have related files
+        tempCount = 0;
+        while (tempCount < spellsList.size()) {
+            logger.info("Going through the spell list: " + spellsList.get(tempCount) + ".json");
+            writeFile(filePath + "\\alex-mcdonald-fyp\\resources\\json\\spells\\" + spellsList.get(tempCount) + ".json", getFile("json/spells/" + spellsList.get(tempCount) + ".json"));
+            tempCount++;
+        }*/
     }
 
     private String getFile(String fileName){
         StringBuilder result = new StringBuilder();
-
+        //logger.info("File name: " + fileName);
         //Get file from resources folder
         ClassLoader classLoader = getClass().getClassLoader();
+        logger.info("Getting file: " + classLoader.getResource(fileName));
         File file = new File(classLoader.getResource(fileName).getFile());
+
         try (Scanner scanner = new Scanner(file)) {
 
             while (scanner.hasNextLine()) {
@@ -93,13 +231,15 @@ public class WriteJSONFiles {
                 result.append(line).append("\n");
             }
 
-            scanner.close();
-
         } catch (IOException e) {
-            logger.error("IOException:" + e);
+            logger.error("IOException getting " + file.getAbsolutePath() + ":" + e);
             e.printStackTrace();
         }
-
+        catch (NullPointerException e) {
+            logger.error("NullPointerException getting " + file.getAbsolutePath() + ":" + e);
+            e.printStackTrace();
+        }
+        logger.info("File gotten: " + fileName);
         return result.toString();
     }
 
@@ -113,7 +253,7 @@ public class WriteJSONFiles {
             bw = new BufferedWriter(fw);
             bw.write(content);
 
-            System.out.println("Done");
+            logger.info("File written to: " + filePath);
 
         } catch (IOException e) {
 
